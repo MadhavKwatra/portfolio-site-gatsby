29d3155a4842f966b708f7fcaff02696054c06b1
MadhavKwatra <madhavkwatra.8427478242@gmail.com>
2024-05-26 13:50:29 +0530

    Updated files file according to my details

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Madhav Kwatra',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Madhav Kwatra is a software developer specializing in backend (and occasionally frontend) digital experiences.',
+    siteUrl: 'https://madhavkwatra.site', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@madhav_kwatra',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
