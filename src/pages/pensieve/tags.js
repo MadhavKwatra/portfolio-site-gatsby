6f8bdad0bd3dcdd0e080a5691887d8d29d9abf69
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2025-01-18 21:47:56 +0530

    Disabled some original creator's pages

--- src/pages/pensieve/tags.js
+++ src/pages/pensieve/tags.js
@@ -1,64 +1,74 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import kebabCase from 'lodash/kebabCase';
+import { useEffect } from 'react';
+import { graphql, navigate } from 'gatsby';
+// import kebabCase from 'lodash/kebabCase';
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
-import { Layout } from '@components';
+// import { Helmet } from 'react-helmet';
+// import styled from 'styled-components';
+// import { Layout } from '@components';
 
-const StyledTagsContainer = styled.main`
-  max-width: 1000px;
+// const StyledTagsContainer = styled.main`
+//   max-width: 1000px;
 
-  h1 {
-    margin-bottom: 50px;
-  }
-  ul {
-    color: var(--light-slate);
+//   h1 {
+//     margin-bottom: 50px;
+//   }
+//   ul {
+//     color: var(--light-slate);
 
-    li {
-      font-size: var(--fz-xxl);
+//     li {
+//       font-size: var(--fz-xxl);
 
-      a {
-        color: var(--light-slate);
+//       a {
+//         color: var(--light-slate);
 
-        .count {
-          color: var(--slate);
-          font-family: var(--font-mono);
-          font-size: var(--fz-md);
-        }
-      }
-    }
-  }
-`;
+//         .count {
+//           color: var(--slate);
+//           font-family: var(--font-mono);
+//           font-size: var(--fz-md);
+//         }
+//       }
+//     }
+//   }
+// `;
 
-const TagsPage = ({
-  data: {
-    allMarkdownRemark: { group },
-  },
-  location,
-}) => (
-  <Layout location={location}>
-    <Helmet title="Tags" />
+// const TagsPage = ({
+//   data: {
+//     allMarkdownRemark: { group },
+//   },
+//   location,
+// }) => {
+const TagsPage = () => {
+  useEffect(() => {
+    navigate('/404'); // Redirect to 404 page
+  }, []);
 
-    <StyledTagsContainer>
-      <span className="breadcrumb">
-        <span className="arrow">&larr;</span>
-        <Link to="/pensieve">All memories</Link>
-      </span>
+  return null; // No content to display, it will redirect
 
-      <h1>Tags</h1>
-      <ul className="fancy-list">
-        {group.map(tag => (
-          <li key={tag.fieldValue}>
-            <Link to={`/pensieve/tags/${kebabCase(tag.fieldValue)}/`} className="inline-link">
-              {tag.fieldValue} <span className="count">({tag.totalCount})</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </StyledTagsContainer>
-  </Layout>
-);
+  // Disabling original creator's tags page
+  // return (
+  //   <Layout location={location}>
+  //     <Helmet title="Tags" />
+
+  //     <StyledTagsContainer>
+  //       <span className="breadcrumb">
+  //         <span className="arrow">&larr;</span>
+  //         <Link to="/pensieve">All memories</Link>
+  //       </span>
+
+  //       <h1>Tags</h1>
+  //       <ul className="fancy-list">
+  //         {group.map(tag => (
+  //           <li key={tag.fieldValue}>
+  //             <Link to={`/pensieve/tags/${kebabCase(tag.fieldValue)}/`} className="inline-link">
+  //               {tag.fieldValue} <span className="count">({tag.totalCount})</span>
+  //             </Link>
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </StyledTagsContainer>
+  //   </Layout>
+  // );
+};
 
 TagsPage.propTypes = {
   data: PropTypes.shape({
